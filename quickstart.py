b0fb55dbe7c8d00b315185113ba6c6be0445187c
zzzachzzz <zach1rosen@gmail.com>
2018-10-06 16:19:36 +0000

    Google Sheets API stuff

---
+++ quickstart.py
@@ -0,0 +1,41 @@
+# from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+# The ID and range of a sample spreadsheet.
+# SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+# SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+    # Call the Sheets API
+    SPREADSHEET_ID = '1DZshSVwHRY-uFnkeBihRn53UkFH6uDEGbxwe80BKl74'
+    RANGE_NAME = 'Settings!1:2'
+    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+                                                range=RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            print(row)
+            # Print columns A and E, which correspond to indices 0 and 4.
+            # print('%s, %s' % (row[0], row[4]))
+
+if __name__ == '__main__':
+    main()
