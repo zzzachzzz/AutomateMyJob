87319c4139076fcdb8796332022f895d0d162feb
zzzachzzz <zach1rosen@gmail.com>
2018-12-12 11:27:21 -0700

    Added option to specify browser driver

--- job/__init__.py
+++ job/__init__.py
@@ -4,25 +4,29 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from pprint import pprint
-# import colorama
-# from colorama import Fore, Back, Style
-# import logging
-# import json
-# import time
-# import re
 from importlib import reload
 
 
-
-driver = webdriver.Chrome()
-# driver = webdriver.Ie()
+use_ie = input()
+if use_ie in {'ie', 'i'}:
+    driver = webdriver.Ie()
+else:
+    driver = webdriver.Chrome()
 wait = webdriver.support.ui.WebDriverWait(driver, 10)
+
 from .alias_util_functions import find_e, find_e_wait, find_all_e, find_all_e_wait, k
 from . import job
 from . import build
 j = job.Job()
 b = build.Build()
-marsha, instance = 'sfonw', '02'
+
+# IPP
+# marsha, instance = 'sfonw', '02'
+# url = 'http://wemprod.marriott.com:27110/content/#/workspace/folder/hotelwebsites/us/' + \
+#        marsha.lower()[0]+'/'+marsha.lower()+'/IPP'+instance
+
+# T3
+marsha = 'tcisi'
 url = 'http://wemprod.marriott.com:27110/content/#/workspace/folder/hotelwebsites/us/' + \
-       marsha.lower()[0]+'/'+marsha.lower()+'/IPP'+instance
+       marsha.lower()[0]+'/'+marsha.lower()+'/Elevated'
 driver.get(url)
