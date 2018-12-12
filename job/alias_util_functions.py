171f80f3195ca53621b753485e7f1c59831982ce
zzzachzzz <zach1rosen@gmail.com>
2018-12-12 11:28:05 -0700

    Reverted parameter name to 'by'

--- job/alias_util_functions.py
+++ job/alias_util_functions.py
@@ -8,20 +8,20 @@ CSS_SELECTOR, XPATH, CLASS_NAME, ID,
 LINK_TEXT, NAME, PARTIAL_LINK_TEXT, TAG_NAME
 """
 
-def find_e(selector, locator=By.CSS_SELECTOR):
-    return driver.find_element(locator, selector)
+def find_e(selector, by=By.CSS_SELECTOR):
+    return driver.find_element(by, selector)
 
 
-def find_e_wait(selector, locator=By.CSS_SELECTOR):
-    return wait.until(EC.presence_of_element_located((locator, selector) ))
+def find_e_wait(selector, by=By.CSS_SELECTOR):
+    return wait.until(EC.presence_of_element_located((by, selector) ))
 
 
-def find_all_e(selector, locator=By.CSS_SELECTOR):
-    return driver.find_elements(locator, selector)
+def find_all_e(selector, by=By.CSS_SELECTOR):
+    return driver.find_elements(by, selector)
 
 
-def find_all_e_wait(selector, locator=By.CSS_SELECTOR):
-    return wait.until(EC.presence_of_all_elements_located((locator, selector) ))
+def find_all_e_wait(selector, by=By.CSS_SELECTOR):
+    return wait.until(EC.presence_of_all_elements_located((by, selector) ))
 
 
 def k():
