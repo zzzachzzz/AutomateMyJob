ae00f3b1c50af76c9be5779b307b0e07080175f0
zzzachzzz <zach1rosen@gmail.com>
2018-12-16 18:09:59 -0700

    Tagging paths with *page_type_locator unpacking in a single class

--- job/tagging_paths.py
+++ job/tagging_paths.py
@@ -1,115 +1,103 @@
-your_marsha = ''
-your_tile = ''
-your_room_pool_code = ''
+from typing import Tuple
 
 
+"""
+If passing page_type_locator containing only one string in the tuple,
+pass parameter as such: tp = TaggingPaths(('string', ), 'marsha')
+"""
 class TaggingPaths:
-    def __init__(self, marsha, tile=''):
+    def __init__(self, page_type_locator: Tuple[str], marsha: str,
+                 tile='', instance='', room_pool_code='', product_id=''):
         self._marsha = marsha
         self._tile = tile
+        self._instance = instance
+        self._room_pool_code = room_pool_code
+        self._product_id = product_id
 
         self.Header = ['Text Length', 'MultiArticle', 'Header']
         self.Body = ['Text Length', 'MultiArticle', 'Body']
         self.Modal = ['Hotel Websites Placement', 'Link', 'Modal']
 
+        self.singleHeroImage = ['HWS Tier 3', *page_type_locator,
+                                'singleHeroImage']
+        self.heroTextOverlay = ['HWS Tier 3', *page_type_locator,
+                                'heroTextOverlay']
+        self.titleSubtitleText = ['HWS Tier 3', *page_type_locator,
+                                  'titleSubtitleText']
+        self.heroTitleLongDescriptionCta = ['HWS Tier 3', *page_type_locator,
+                                            'heroTitleLongDescriptionCta']
+
+        self.roomLongDescription = ['HWS Tier 3', *page_type_locator,
+                                    'roomLongDescription']
+        self.headingTextListOfArticles = ['HWS Tier 3', *page_type_locator,
+                                          'headingTextListOfArticles']
+        self.headerTextCta = ['HWS Tier 3', *page_type_locator,
+                              'headerTextCta']
+        self.titleImage = ['HWS Tier 3', *page_type_locator, 'titleImage']
+
     @property
     def marsha(self):
         return ['MARSHA Codes', self._marsha[0], self._marsha]
-
-
-# Accepts landing_layout 'A' or 'B'
-class LandingPage(TaggingPaths):
-    def __init__(self, landing_layout, marsha, tile='', instance=''):
-        super().__init__(marsha, tile)
-        self._landing_layout = landing_layout
-        self._instance = instance
-
-        self.heroTextOverlay = ['HWS Tier 3', 'Landing Page', self._landing_layout, 'heroTextOverlay']
-        self.titleSubtitleText = ['HWS Tier 3', 'Landing Page', self._landing_layout, 'titleSubtitleText']
-
-    @property
-    def heroImageHeaderTextCta(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'heroImageHeaderTextCta', self._tile]
-
-    @property
-    def imageClusterHeadingTextCta(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'imageClusterHeadingTextCta', self._tile]
-
-    @property
-    def imageTextCarousel(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'imageTextCarousel', self._tile]
 
     @property
     def instance(self):
         return ['Instance', self._instance]
 
     @property
+    def room_pool_code(self):
+        return ['Property Information', 'Room Codes',
+                'Room Pool Codes', self._room_pool_code]
+
+    # TODO Needs work
+    # Delegate product ID matching to a function in job.py
+    @property
+    def product_id(self):
+        # page_type_locator would unpack something like:
+        # ('Products Dining', ) or ('Products Spa', )
+        return [*page_type_locator, self._marsha[0],
+                self._marsha, self._product_id]
+
+    @property
+    def heroImageHeaderTextCta(self):
+        return ['HWS Tier 3', *page_type_locator,
+                'heroImageHeaderTextCta', self._tile]
+
+    @property
+    def imageClusterHeadingTextCta(self):
+        return ['HWS Tier 3', *page_type_locator,
+                'imageClusterHeadingTextCta', self._tile]
+
+    @property
+    def imageTextCarousel(self):
+        return ['HWS Tier 3', *page_type_locator,
+                'imageTextCarousel', self._tile]
+
+    @property
     def imageHeaderTextCtaAdvanced(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'imageHeaderTextCtaAdvanced', self._tile]
+        return ['HWS Tier 3', *page_type_locator,
+                'imageHeaderTextCtaAdvanced', self._tile]
     
     @property
     def dividerImageText(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'dividerImageText', self._tile]
+        return ['HWS Tier 3', *page_type_locator,
+                'dividerImageText', self._tile]
     
     @property
     def imageMosaicHeadingTextCta(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'imageMosaicHeadingTextCta', self._tile]
+        return ['HWS Tier 3', *page_type_locator,
+                'imageMosaicHeadingTextCta', self._tile]
 
     # Possibly need different tag for Layout B
     # 'imageTextCtaCarouselDining'
     @property
     def imageTextCtaCarousel(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'imageTextCtaCarousel', self._tile]
+        return ['HWS Tier 3', *page_type_locator,
+                'imageTextCtaCarousel', self._tile]
 
     @property
     def mapWhatsNearby(self):
-        return ['HWS Tier 3', 'Landing Page', self._landing_layout, 'mapWhatsNearby']
-    
-    
-class HotelOverview(TaggingPaths):
-    def __init__(self, marsha, locator, tile=''):
-        super().__init__(marsha, tile)
-
-        self.singleHeroImage = ['HWS Tier 3', *locator, 'singleHeroImage']
-
-
-    
-    
-
-class RoomsAndSuites(TaggingPaths):
-    def __init__(self, marsha, tile=''):
-        super().__init__(marsha, tile)
-
-        self.singleHeroImage = ['HWS Tier 3', 'Rooms & Suites', 'singleHeroImage']
-        self.heroTitleLongDescriptionCta = ['HWS Tier 3', 'Rooms & Suites', 'heroTitleLongDescriptionCta']
-    
-    @property
-    def imageMosaicHeadingTextCta(self):
-        return ['HWS Tier 3', 'Rooms & Suites', 'imageMosaicHeadingTextCta', self._tile]
-    
-    @property
-    def imageHeaderTextCtaAdvanced(self):
-        return ['HWS Tier 3', 'Rooms & Suites', 'imageHeaderTextCtaAdvanced', self._tile]
-
-
-class RoomDetails(RoomsAndSuites):
-    def __init__(self, marsha, room_pool_code, tile=''):
-        super().__init__(marsha, tile)
-        self._room_pool_code = room_pool_code
-
-        self.singleHeroImage = ['HWS Tier 3', 'Rooms & Suites', 'Room Details', 'singleHeroImage']
-        self.roomLongDescription = ['HWS Tier 3', 'Rooms & Suites', 'Room Details', 'roomLongDescription']
-        self.headingTextListOfArticles = ['HWS Tier 3', 'Rooms & Suites', 'Room Details', 'headingTextListOfArticles']
-        self.headerTextCta = ['HWS Tier 3', 'Rooms & Suites', 'Room Details', 'headerTextCta']
-        self.titleImage = ['HWS Tier 3', 'Rooms & Suites', 'Room Details', 'titleImage']
-
-    @property
-    def room_pool_code(self):
-        return ['Property Information', 'Room Codes', 'Room Pool Codes', self._room_pool_code]
+        return ['HWS Tier 3', *page_type_locator, 'mapWhatsNearby']
 
     @property
     def video(self):
-        return ['HWS Tier 3', 'Rooms & Suites', 'Room Details', 'video', self._tile]
-    
-    
-    
+        return ['HWS Tier 3', *page_type_locator, 'video', self._tile]
