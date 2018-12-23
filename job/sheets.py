524712f1761506504c923024d93632ff8679a624
zzzachzzz <zach1rosen@gmail.com>
2018-12-23 05:44:08 +0000

    qs.py -> sheets.py | Now gets all sheet names and generates ranges.

---
+++ job/sheets.py
@@ -0,0 +1,67 @@
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from pprint import pprint
+from typing import List
+from . import tagging_paths
+from . import marsha
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+# SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1H_YWddeM8KaYQf7chWodWb1zUF8tjFz2qxvoKpNYztY'
+# RANGE_NAME = 'Hotel Overview!D1:D100'
+
+def main():
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    # spreadsheet = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID).execute()
+    # sheets = spreadsheet.get('sheets')
+    # sheet_names = get_sheet_names(sheets)
+
+    RANGE_NAME = generate_range_name('Hotel Overview') # sheet
+    # pprint(sheet_names)
+    result = service.spreadsheets().values().get(
+        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    # body = {
+    #     'values': [['test']]
+    # }
+
+    # result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
+    #     valueInputOption='USER_ENTERED', body=body).execute()
+    values = result.get('values')
+    pprint(values)
+    parsed = parse_for_content()
+    
+    return
+
+
+def generate_range_name(sheet: str) -> str:
+    if sheet == 'Landing Page B':
+        range_name = sheet + '!N:N'
+    else:
+        range_name = sheet + '!D:D'
+    print("range_name:", range_name)
+    return range_name
+
+
+def get_sheet_names(sheets: list) -> list:
+    sheet_names = []
+    for s in sheets:
+        title = s.get('properties').get('title')
+        sheet_names.append(title)
+    return sheet_names
+
+def parse_for_content(sheet_title: str, values: List[List[str]]):
+    tp = tagging_paths.TaggingPaths(sheet_title, marsha)
+    # :)))))
+
+
+if __name__ == '__main__':
+    main()
