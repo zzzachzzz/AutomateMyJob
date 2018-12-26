a985f98977c42c7a317e3ba3a1769adfb8e1fdcc
zzzachzzz <zach1rosen@gmail.com>
2018-12-26 03:04:37 +0000

    Build information now stored in Build class, referenced by CIF class names attribute.

--- job/build.py
+++ job/build.py
@@ -1,7 +1,4 @@
-# 'Visual Navigation ( E )': 'unknown' WHO ARE YOU
-# perhaps 'imageHeaderTextCta' ?
-
-# 'name': marsha+'_'+sheet_title+'_imageMosaicHeadingTextCta_TileA_Article_Body',
+import tagging_paths
 
 content_identifiers = {
     'Image URL',
@@ -21,7 +18,137 @@ content_identifiers = {
 }
 
 
-Build = {
+
+class Build:
+    def __init__(self, sheet_title, marsha):
+        self.tp = tagging_paths.TaggingPaths(sheet_title, marsha)
+        self.sheet_title = sheet_title.replace(' ', '')
+        self._marsha = marsha
+
+    def singleHeroImage(self):
+        return [
+            {
+                'type': 'wrapper',
+                'name': self._marsha+'_'+self.sheet_title+'_singleHeroImage',
+                'tags': [self.tp.singleHeroImage, self.tp.marsha],
+                'ref': [
+                    'Image URL',
+                ],
+            },
+        ]
+
+    def titleSubtitleText(self):
+        return [
+            {
+                'type': 'article',
+                'name': self._marsha+'_'+self.sheet_title+'_titleSubtitleText',
+                'tags': [self.tp.titleSubtitleText, self.tp.marsha],
+                'title': 'Page Subhead (30-60 characters )',
+                'body': 'Intro Copy (No character limit)',
+            },
+        ]
+
+    def imageClusterHeadingTextCta(self, tile):
+        return [
+            {
+                'type': 'wrapper',
+                'name': self._marsha+'_'+self.sheet_title+'_imageClusterHeadingTextCta_'+tile.replace(' ', ''),
+                'tags': [self.tp.imageClusterHeadingTextCta(tile), self.tp.marsha],
+                'ref': [
+                    'Main Image URL',
+                    'Bottom Left Image URL',
+                    'Bottom Center Image URL',
+                    'Bottom Right Image URL',
+                ],
+            },
+            {
+                'type': 'article',
+                'name': self._marsha+'_'+self.sheet_title+'_imageClusterHeadingTextCta_'+tile.replace(' ', '')+'_Article_Header',
+                'tags': [self.tp.imageClusterHeadingTextCta(tile), self.tp.Header, self.tp.marsha],
+                'title': 'Card Title (60-80 characters)',
+            },
+            {
+                'type': 'article',
+                'name': self._marsha+'_'+self.sheet_title+'_imageClusterHeadingTextCta_'+tile.replace(' ', '')+'_Article_Body',
+                'tags': [self.tp.imageClusterHeadingTextCta(tile), self.tp.Body, self.tp.marsha],
+                'title': 'Card Subhead (30-60 characters)',
+                'body': 'Card Body Copy (144-160 characters)',
+            },
+        ]
+
+    def imageHeaderTextCtaAdvanced(self, tile):
+        return [
+            {
+                'type': 'article',
+                'name': self._marsha+'_'+self.sheet_title+'_imageHeaderTextCtaAdvanced_'+tile.replace(' ', '')+'_Article',
+                'title': 'Card Title (60-80 characters)',
+                'body': 'Card Body Copy (144-160 characters)'
+            },
+            {
+                'type': 'wrapper',
+                'name': self._marsha+'_'+self.sheet_title+'_imageHeaderTextCtaAdvanced_'+tile.replace(' ', ''),
+                'tags': [self.tp.imageHeaderTextCtaAdvanced(tile), self.tp.marsha],
+                'ref': [
+                    'Main Image URL',
+                    self._marsha+'_'+self.sheet_title+'_imageHeaderTextCtaAdvanced_'+tile.replace(' ', '')+'_Article',
+                ]
+
+            },
+        ]
+
+    def imageTextCarousel(self, tile, instance):
+        return [
+            {
+                'type': 'article',
+                'name': self._marsha+'_'+self.sheet_title+'_imageTextCarousel_'+tile.replace(' ', '')+'_'+instance+'_Article',
+                'title': 'Card Title (60-80 characters)',
+                'body': 'Card Body Copy (144-160 characters)'
+            },
+            {
+                'type': 'wrapper',
+                'name': self._marsha+'_'+self.sheet_title+'_imageTextCarousel_'+tile.replace(' ', '')+'_'+instance,
+                'tags': [self.tp.imageTextCarousel(tile), self.tp.instance(instance), self.tp.marsha],
+                'ref': [
+                    'Card Image URL',
+                    self._marsha+'_'+self.sheet_title+'_imageTextCarousel_'+tile.replace(' ', '')+'_'+instance+'_Article',
+                ]
+
+            },
+        ]
+
+
+
+# Will later reference articles / images by matching name to vcmid
+# name: 'VCM9302834290483ID'
+
+# Links to be handled in the fuuuture
+
+
+# Content Intake Form Names
+class CIF:
+    def __init__(self, b: Build):
+        self.names = {
+            'Hero Image': b.singleHeroImage(),
+            'Intro (A)': b.titleSubtitleText(),
+            'Primary Feature Card 1 (B)': b.imageClusterHeadingTextCta('Tile A'),
+            'Primary Feature Card 2 (B)': b.imageClusterHeadingTextCta('Tile B'),
+            'Secondary Feature Card 1': b.imageHeaderTextCtaAdvanced('Tile A'),
+            'Secondary Feature Card 2': b.imageHeaderTextCtaAdvanced('Tile B'),
+            'Secondary Feature Card 3': b.imageHeaderTextCtaAdvanced('Tile C'),
+            'Secondary Feature Card 4': b.imageHeaderTextCtaAdvanced('Tile D'),
+            'Secondary Feature Card 5': b.imageHeaderTextCtaAdvanced('Tile E'),
+            'Secondary Feature Card 6': b.imageHeaderTextCtaAdvanced('Tile F'),
+            'Secondary Feature Card 7': b.imageHeaderTextCtaAdvanced('Tile G'),
+            'Secondary Feature Card 8': b.imageHeaderTextCtaAdvanced('Tile H'),
+            'Secondary Feature Card 9': b.imageHeaderTextCtaAdvanced('Tile I'),
+            'Secondary Feature Card 10': b.imageHeaderTextCtaAdvanced('Tile J'),
+            'Messaging Carousel Card 1':  b.imageTextCarousel('Tile A', '01'),
+            'Messaging Carousel Card 2':  b.imageTextCarousel('Tile A', '02'),
+            'Messaging Carousel Card 3':  b.imageTextCarousel('Tile A', '03'),
+        }
+
+
+OLD_Build = {
     'Landing Page B': {
         'Hero Image/Video': {
             'type': 'heroTextOverlay'
