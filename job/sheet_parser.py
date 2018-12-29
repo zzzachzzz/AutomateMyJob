391a16a63e7e5e6a615ba7110c2a91a1b4820d06
zzzachzzz <zach1rosen@gmail.com>
2018-12-29 19:36:08 +0000

    Sheet parsing algorithm complete for preparing build sequence

--- job/sheet_parser.py
+++ job/sheet_parser.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 import pickle
-from typing import List
+import json
+import re
+from typing import List, Set
 from job import build
 from job import cif_names_to_build
 from job.tagging_paths import get_page_type
@@ -11,28 +13,86 @@ def to_html():
     return
 
 
-def parse_for_content(sheet_title: str, marsha: str, values: List[List[str]]) -> list:
-    def complete_build_sequence_piece():
-        pass
+# Removes elements from components where content was not added.
+# Skips appending the component to the cleaned list if the component
+# had no content added at all.
+def complete_build_section(build_section: List[dict], content_identifiers: Set[str]) -> List[dict]:
+    cleaned_build_section = []
+    for component in build_section:
+        cleaned_build_component = dict(component)  # Create copy of dict for modifying
+
+        if component['type'] == 'article':
+            if component.get('title') and component['title'] in content_identifiers:
+                del cleaned_build_component['title']
+            if component.get('body') and component['body'] in content_identifiers:
+                del cleaned_build_component['body']
+            if (cleaned_build_component.get('body') or cleaned_build_component.get('title')):
+                cleaned_build_section.append(cleaned_build_component)
+
+        elif component['type'] == 'wrapper':
+            cleaned_build_component['ref'] = [x for x in component['ref'] if x not in content_identifiers]
+            if len(cleaned_build_component['ref']) > 0:
+                cleaned_build_section.append(cleaned_build_component)
+
+        elif component['type'] == 'image':
+            if component['image'] not in content_identifiers:
+                cleaned_build_section.append(cleaned_build_component)
+
+    return cleaned_build_section
+
+
+def get_content_identifiers(build_section: List[dict]) -> Set[str]:
+    content_identifiers = set()
+    for component in build_section:
+        if component['type'] == 'article':
+            if component.get('title'):
+                content_identifiers.add(component['title'])
+            if component.get('body'):
+                content_identifiers.add(component['body'])
+        elif component['type'] == 'wrapper':
+            for x in component['ref']:
+                content_identifiers.add(x)
+        elif component['type'] == 'image':
+            content_identifiers.add(component['image'])
+    return content_identifiers
+
+
+def parse_for_content(sheets_api_response: dict, marsha: str) -> list:
+    values = sheets_api_response['values']
+    sheet_title = re.search("[^\']*[^\']",
+                       sheets_api_response['range']).group()
+    print(sheet_title)
+    values = [x[0] if len(x) > 0 else '' for x in values]
+    # image_replacements = [x[2] if len(x) > 2 else '' for x in values]
+
+
 
     b = build.Build(sheet_title, marsha)
     # Get the specific subclass of CIF needed based on page_type
     page_type = get_page_type(sheet_title)
-    TargetClass = getattr(cif_names_to_build, page_type.replace(' ', '') )
+    # Get subclass according to page_type, otherwise
+    # default to the base class CIF.
+    TargetClass = getattr(cif_names_to_build, page_type.replace(' ', ''),
+                          cif_names_to_build.CIF)  # Last argument is default
     cif = TargetClass(b)
 
-
-    content_identifiers = build.content_identifiers
     build_sequence = []
+    content_identifiers = set()
     i = 0
     # while i <= len(values)-1:
     while i <= 123:
-        # If blank cell, shouldn't be any after previous filter
         if values[i] == '':
             i += 1
 
         elif values[i] in cif.names:
+            if len(build_sequence) > 0:
+                build_sequence[-1] = complete_build_section(build_sequence[-1], content_identifiers)
+                # If empty list was appended, delete it
+                if len(build_sequence[-1]) == 0:
+                    del build_sequence[-1]
+
             build_sequence.append( cif.names[values[i]]() )
+            content_identifiers = get_content_identifiers(build_sequence[-1])
             i += 1
 
         # If content identifier
@@ -47,19 +107,32 @@ def parse_for_content(sheet_title: str, marsha: str, values: List[List[str]]) ->
                     if values[i] in component['ref']:
                         ref_index = component['ref'].index(values[i])
                         component['ref'][ref_index] = values[i+1]
+                elif component['type'] == 'image':
+                    component['image'] = values[i+1]
+                else:
+                    print("Error :thinking:")
             i += 2
 
         else:
             i += 1
+    if len(build_sequence) > 0:
+        build_sequence[-1] = complete_build_section(build_sequence[-1], content_identifiers)
+    # If empty list was appended, delete it
+    if len(build_sequence[-1]) == 0:
+        del build_sequence[-1]
 
     return build_sequence
 
 
 if __name__ == '__main__':
-    with open('job/values.pickle', 'rb') as pickle_file:
-        values = pickle.load(pickle_file)
-    pprint(values)
-    print("\n\n")
-    parsed = parse_for_content('Hotel Overview', 'TCISI', values)
-    print("\n\n\nFinal:")
+    # with open('job/values.pickle', 'rb') as pickle_file:
+    #     values = pickle.load(pickle_file)
+    # pprint(values)
+    # print("\n\n")
+    # parsed = parse_for_content('Hotel Overview', 'TCISI', values)
+    # print("\n\n\nFinal:")
+    # pprint(parsed)
+    with open('job/resultSample.json', 'r') as f:
+        sheets_api_response = json.load(f)
+    parsed = parse_for_content(sheets_api_response, 'TCISI')
     pprint(parsed)
